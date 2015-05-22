0bdb60ab0870bad277b926c813dead2be605d425
karantabrizi <karan.tabrizi@photoninfotech.net>
2015-05-22 14:19:16 -0500

    hi

---
+++ JCPtest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JCPtest
+//
+//  Created by karan tabrizi on 5/22/15.
+//  Copyright (c) 2015 karan tabrizi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JCPtest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JCPtest
+//
+//  Created by karan tabrizi on 5/22/15.
+//  Copyright (c) 2015 karan tabrizi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
